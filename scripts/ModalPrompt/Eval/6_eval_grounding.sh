8c96aa9d4dcb329f6912491fe9bbe5ad24b3c5e9
Fanhu Zeng <119220964+AuroraZengfh@users.noreply.github.com>
2025-04-14 16:21:54 +0800

    Add files via upload

---
+++ scripts/ModalPrompt/Eval/6_eval_grounding.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CHUNKS=1
+IDX=0
+
+STAGE=$1
+MODELPATH=$2
+CUR_TASK=$3
+RESULT_DIR="./results/ModalPrompt/Grounding"
+
+CUDA_VISIBLE_DEVICES=0 python -m llava.eval.ModalPrompt.model_vqa_cc_instruction \
+    --model-path $MODELPATH \
+    --model-base models/llava_v1.5-7b \
+    --question-file instructions/Grounding/test.json \
+    --image-folder datasets/ \
+    --text-tower models/clip-vit-large-patch14-336 \
+    --prefix-len 10 \
+    --cur-task $CUR_TASK \
+    --answers-file $RESULT_DIR/$STAGE/${CHUNKS}_${IDX}.jsonl \
+    --num-chunks $CHUNKS \
+    --chunk-idx $IDX \
+    --temperature 0 \
+    --conv-mode vicuna_v1 &
+
+wait
+
+output_file=$RESULT_DIR/$STAGE/merge.jsonl
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat $RESULT_DIR/$STAGE/${CHUNKS}_${IDX}.jsonl >> "$output_file"
+done
+
+python -m llava.eval.ModalPrompt.eval_grounding \
+    --test-file instructions/Grounding/test.json \
+    --result-file $output_file \
+    --output-dir $RESULT_DIR/$STAGE \
---
+++ scripts/ModalPrompt/Train/4_GQA.sh
@@ -0,0 +1,50 @@
+################## VICUNA ##################
+PROMPT_VERSION=v1
+MODEL_VERSION="vicuna-7b-v1.5"
+################## VICUNA ##################
+
+
+################## LLaMA-2 ##################
+# PROMPT_VERSION="llava_llama_2"
+# MODEL_VERSION="Llama-2-7b-chat-hf"
+################## LLaMA-2 ##################
+
+deepspeed --include localhost:0,1,2,3 --master_port 13200 llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3_offload.json \
+    --lora_enable False --mm_projector_lr 2e-5 --pt_enable True \
+    --model_name_or_path models/llava_v1.5-7b \
+    --previous_task_model_path checkpoints/ModalPrompt/ImageNet/llava-1.5-7b \
+    --version $PROMPT_VERSION \
+    --data_path instructions/GQA/train.json \
+    --image_folder datasets \
+    --vision_tower models/clip-vit-large-patch14-336 \
+    --text_tower models/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter models/llava_v1.5-7b/mm_projector.bin \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_text_select_layer -1 \
+    --prefix_len 10 \
+    --cur_task 4 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir checkpoints/ModalPrompt/GQA/llava-1.5-7b \
+    --num_train_epochs 10 \
+    --per_device_train_batch_size 24 \
+    --per_device_eval_batch_size 16 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "epoch" \
+    --learning_rate 2e-4 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to none
